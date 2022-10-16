c4836817a08f9aab78c10d947f820f075de869f4
Mark Bydder <17042435+marcsous@users.noreply.github.com>
2022-10-16 13:48:59 -0700

    Add files via upload

--- examples.m
+++ examples.m
@@ -6,11 +6,10 @@ clear all; close all;
 %load phantom1 % 2x undersampled
 %load phantom2 % 2x undersampled
 load phantom3 % 2x undersampled
-fprintf('Added errors(%i) =',numel(added_errors)); fprintf(' %i',added_errors); fprintf('.\n');
-figure; take2(data,mask);
+figure; take2(data,'std',0.08,'errors',added_errors);
 
 %% in vivo data
 %load head1 % fully sampled
 %load head2 % fully sampled
 load head3 % fully sampled
-figure; take2(data,mask);
+figure; take2(data);
--- take2.m
+++ take2.m
@@ -1,22 +1,21 @@
-function [ksp mask flag] = take2(data,mask,varargin)
-% [ksp mask flag] = take2(data,mask,varargin)
+function [ksp mask flag] = take2(data,varargin)
+% [ksp mask flag] = take2(data,varargin)
 %
 % Trimmed autocalibrating k-space estimation in 2D
 % based on structured low rank matrix completion.
-% Uses a heuristic approaches to remove outliers.
+% Uses an heuristic approach to remove outliers.
 %
-% Singular value filtering requires noise estimate
-% which is a key parameter. The other key parameter
-% is tolerance (smaller is better but slower).
+% Singular value filtering requires the noise std
+% which is a key parameter. Other key parameters
+% are tolerance (smaller is better/slower) and no.
+% stds to define the outlier threshold.
 %
 % Inputs:
 %  -data [nx ny nc]: kspace data from nc coils
-%  -mask: 1D/2D/3D sampling mask (or 1D indices)
-%  -varargin: option/value pairs (e.g. 'nmad',4)
+%  -varargin: option/value pairs (e.g. 'nstd',4)
 %
 % Outputs:
 %  -ksp [nx ny nc]: kspace data from nc coils
-%  -mask [nx ny nc]: updated sampling mask
 %  -flag is zero for successful termination
 %
 % References:
@@ -27,19 +26,17 @@ function [ksp mask flag] = take2(data,mask,varargin)
 %% setup
 
 % default options
-opts.width = 5; % kernel width (default 5)
-opts.radial = 0; % use radial kernel (0 or 1)
-opts.loraks = 0; % use phase constraint (0 or 1)
-opts.tol = 5e-4; % relative tolerance (5e-4)
+opts.width = 4; % kernel width (default 4)
+opts.radial = 1; % use radial kernel (0 or 1)
+opts.loraks = 1; % use phase constraint (0 or 1)
+opts.tol = 1e-4; % relative tolerance (1e-4)
 opts.maxit = 1e4; % maximum no. iterations (1e4)
-opts.minit = 1e1; % minimum no. iterations (1e1)
-opts.irls = 5; % no. irls iterations (0=use mean)
-opts.nmad = 5; % outlier threshold (mad=1.4826*std)
-opts.proj = 2; % projection dimension (0, 1 or 2)
-opts.noise = []; % noise std, if available
-opts.center = []; % center of kspace, if available
+opts.minit = 10; % minimum no. iterations (10)
+opts.irls = 3; % no. irls iterations (0=mean)
+opts.nstd = 4; % outlier threshold (no. std devs)
+opts.readout = 2; % readout dimension (0, 1 or 2)
+opts.std = []; % noise std dev, if available
 opts.errors = []; % known errors (for validation)
-opts.display = 20; % no. iterations between plots
 opts.power = 0.5; % density weighting power (0=off)
 
 % varargin handling (must be option/value pairs)
@@ -48,41 +45,22 @@ for k = 1:2:numel(varargin)
         error('''varargin'' must be option/value pairs.');
     end
     if ~isfield(opts,varargin{k})
-        warning('''%s'' is not a valid option.',varargin{k});
+        error('''%s'' is not a valid option.',varargin{k});
     end
     opts.(varargin{k}) = varargin{k+1};
 end
 
 %% initialize
 
-% argument checks - try to be flexible
-if ndims(data)<2 || ndims(data)>3
-    error('Argument ''data'' must be a 3d array.')
+% argument checks
+if ndims(data)<2 || ndims(data)>3 || ~isfloat(data) || isreal(data)
+    error('Argument ''data'' must be a 3d complex float array.')
 end
 [nx ny nc] = size(data);
 
-if ~exist('mask','var') || isempty(mask)
-    mask = any(data,3); % 2d mask [nx ny]
-    warning('''mask'' not supplied - guessing.')
-elseif isvector(mask)
-    if any(mask~=0 & mask~=1) 
-        if any(mask<1 | mask>ny | mod(mask,1))
-            error('''mask'' is incompatible.');
-        end
-        index = mask; % linear indices
-        if opts.proj<=1; mask = false(1,ny); end
-        if opts.proj==2; mask = false(nx,1); end
-        mask(index) = 1; % binary mask
-    end
-    if opts.proj<=1; mask = repmat(reshape(mask,1,ny),[nx 1]); end
-    if opts.proj==2; mask = repmat(reshape(mask,nx,1),[1 ny]); end
-end
-if nnz(mask~=0 & mask~=1); error('''mask'' is incompatible.'); end
-mask = reshape(mask==1,[nx ny]); % ensure size/class compatibility
-
-% projection dimension
-if ~isscalar(opts.proj) || opts.proj<0 || opts.proj>2 | mod(opts.proj,1)
-    error('projection can only be 0, 1 or 2.');
+% readout dimension
+if ~ismember(opts.readout,[0 1 2])
+    error('readout can only be 0, 1 or 2.');
 end
 
 % convolution kernel indicies
@@ -97,20 +75,26 @@ opts.kernel.x = x(k);
 opts.kernel.y = y(k);
 opts.kernel.mask = k;
 
-% dimensions of the matrix
-opts.dims = [nx ny nc nk 1+opts.loraks];
-
 % estimate center of kspace
-if isempty(opts.center)
-    [~,k] = max(reshape(data,[],nc));
-    [x y] = ind2sub([nx ny],k);
-    opts.center(1) = round(gather(median(x)));
-    opts.center(2) = round(gather(median(y)));
-end
+[~,k] = max(reshape(data,[],nc));
+[x y] = ind2sub([nx ny],k);
+opts.center(1) = round(gather(median(x)));
+opts.center(2) = round(gather(median(y)));
 
 % indices for conjugate reflection about center
 opts.flip.x = circshift(nx:-1:1,[0 2*opts.center(1)-1]);
 opts.flip.y = circshift(ny:-1:1,[0 2*opts.center(2)-1]);
+
+% dimensions of the matrix
+opts.dims = [nx ny nc nk 1+opts.loraks];
+
+% sampling mask (same for all coils)
+mask = any(data,3);
+
+% estimate noise std (heuristic)
+std_estimated = isempty(opts.std);
+if std_estimated; opts.std = estimate_std(data,mask); end
+noise_floor = opts.std * sqrt(nnz(mask));
 
 % density
 matrix_density = nnz(mask) / numel(mask);
@@ -124,28 +108,24 @@ fprintf('Matrix density = %f\n',matrix_density);
 try
     gpu = gpuDevice;
     if verLessThan('matlab','8.4'); error('GPU needs MATLAB R2014b.'); end
-    data = gpuArray(data);
     mask = gpuArray(mask);
+    data = gpuArray(data);
     fprintf('GPU found: %s (%.1f Gb)\n',gpu.Name,gpu.AvailableMemory/1e9);
 catch ME
-    data = gather(data);
     mask = gather(mask);
+    data = gather(data);
     warning('%s Using CPU.', ME.message);
 end
 
 %% Cadzow algorithm - solve for ksp
 
-t = tic;
-old = NaN;
-converged = 0;
-noise_floor = -1;
-
-ksp = zeros(size(data),'like',data);
+ksp = data; 
 
 for iter = 1:opts.maxit
     
     % data consistency
-    ksp = bsxfun(@times,data,mask)+bsxfun(@times,ksp,~mask);
+    old = ksp; % old ksp to check convergence
+    ksp = ksp + bsxfun(@times,data-ksp,mask);
     
     % make calibration matrix
     A = make_data_matrix(ksp,opts);
@@ -154,150 +134,95 @@ for iter = 1:opts.maxit
     [V W] = svd(A'*A);
     W = sqrt(diag(W));
     
-    % estimate noise floor of singular values (heuristic)
-    if ~isempty(opts.noise)
-        noise_floor = opts.noise * sqrt(matrix_density*nx*ny);
-    elseif noise_floor==-1
-        for j = 1:numel(W)
-            h = hist(W(j:end));
-            [~,k] = max(h);
-            if k>1; break; end
-        end
-        noise_floor = median(W(j:end)); % center of Marcenko-Pastur distribution?
-    end
-    noise_std = noise_floor / sqrt(matrix_density*nx*ny); % noise std estimate
-
     % minimum variance filter (De Moor B. IEEE Trans Sig Proc 1993;41:2826)
     f = max(0,1-noise_floor.^2./W.^2); 
-    F = V * diag(f) * V';
-    A = A * F;
+    A = A * (V * diag(f) * V');
 
     % undo Hankel stucture
     A = undo_data_matrix(A,opts);
-    
-    % A should be rank nc, combine redundant copies
-    A = reshape(A,nx,ny,nc,[]);
-    if opts.irls==0
-        ksp = mean(A,4);
-    else
-        for j = 1:opts.irls
-            w = abs(bsxfun(@minus,A,ksp));
-            w = 1./max(w,noise_std*1e-7);
-            ksp = sum(w.*A,4)./sum(w,4);
-        end
+
+    % combine redundant copies (mean or huber)
+    ksp = mean(A,4);
+    for j = 1:opts.irls
+        w = abs(bsxfun(@minus,A,ksp));
+        w = 1./hypot(w,opts.std*1e-2); % "small" tuning parameter
+        ksp = sum(w.*A,4) ./ sum(w,4);
     end
 
-    % check convergence: min(normA) not adequate so use tol
-    normA(iter) = sum(W);
-    tol(iter) = norm(ksp(:)-old(:)) / norm(ksp(:));
-    converged = converged + (tol(iter) < opts.tol);
-    old = ksp;
+    % convergence metrics: nuclear norm and delta ksp
+    nrm(1,iter) = sum(W);
+    nrm(2,iter) = norm(ksp(:)-old(:)) / norm(ksp(:));
+    converged = nrm(2,iter)<opts.tol && (iter-t(end))>=opts.minit;
     
-    % residual... or possibly abs(abs(data)-abs(ksp))?
-    r = abs(data-ksp);
- 
+    % residual: abs(data-ksp) is too(?) sensitive to phase errors
+    r = abs(abs(ksp)-abs(data));
+
     % project over coil dimension
-    r = sum(r,3);
+    r = sum(r,3) .* mask;
 
-    % heuristic to inhibit rejection of too many neigboring lines
-    r = (r-median(r(mask))) .* power(sample_density,opts.power);
+    % heuristic to inhibit rejection of neigbors
+    r = r .* power(sample_density,opts.power);
 
-    % project over kspace dimension
-    if opts.proj
-        r = sum(r,opts.proj);
-    end
+    % project over readout dimension
+    if opts.readout; r = sum(r,opts.readout); end
 
-    % center and normalize
+    % center and normalize by (robust) std
     r = reshape(r,[],1);
     [k,~,v] = find(r);
     r(k) = r(k) - median(v);
-    mad = median(abs(r(k)));
-    r = r / mad;
+    rstd = 1.4826 * median(abs(r(k)));
+    r = r / rstd;
 
-    % find the worst data
-    [nmad reject] = max(r);
+    % trim the worst data  
+    if converged
 
-    % trim the worst data
-    if converged==opts.minit
-        
-        if nmad < opts.nmad
+        % find the worst data
+        [nstd reject] = max(r);
+
+        if nstd < opts.nstd
             reject = 0; % trim nothing
         else
-            if opts.proj==0; [x y] = ind2sub([nx ny],reject); end % point
-            if opts.proj==1; y = reject; x = ':'; end % col
-            if opts.proj==2; x = reject; y = ':'; end % row
-            mask(x,y) = 0; % trim point/col/row
-            
-            % catastrophic failure (center of kspace gone!)
-            idx = mod(opts.center(1)+opts.kernel.x-1,nx)+1;
-            idy = mod(opts.center(2)+opts.kernel.y-1,ny)+1;
-            if nnz(mask(idx,idy))==0; reject = -1; end
-        end
-        
-        % display trim info
-        if ~exist('ntrim','var')
-            ntrim = 1; % counter for no. trimmed points/cols/rows
-            disp('----------------------------------------------------------');
-            disp('Count  ||A||   Iter  Trimmed nmad    mad      Noise   Time');
-            disp('----------------------------------------------------------');
-        end
-        fprintf('%3i %9.2e %5i',ntrim,normA(iter),iter);
-        if isempty(opts.errors)
-            fprintf(' %6i  ',reject);
-        else
-            fprintf('%6i(%1i)',reject,ismember(reject,opts.errors));
-        end
-        fprintf('%5.1f %9.2e %9.2e %4.0f\n',min(nmad,99.9),mad,noise_std,toc(t));
+            if opts.readout==2; x = reject; y = ':'; end % row
+            if opts.readout==1; y = reject; x = ':'; end % col
+            if opts.readout==0; [x y] = ind2sub([nx ny],reject); end % point
 
-        % update parameters if we trimmed something
-        if reject>0
-            ntrim = ntrim+1; % increment counter
+            % update parameters
+            mask(x,y) = 0;            
+            t(end+1) = iter;
             matrix_density = nnz(mask) / numel(mask);
             sample_density = calc_sample_density(mask,opts);
-            converged = 0; % reset minit counter            
-            noise_floor = -1; % trigger recalculation
+            if std_estimated; opts.std = estimate_std(data,mask); end
+            noise_floor = opts.std * sqrt(nnz(mask)); r(reject) = 0;
         end
-        
     end
-    
-    % display plots
-    if mod(iter,opts.display)==1 || converged==opts.minit
-        % singular values
-        subplot(2,4,[1 5]); plot(W/W(1)); hold on; plot(f,'--'); hold off
-        xlim([0 numel(f)+1]); title(sprintf('rank %i/%i',nnz(f),numel(f)));
-        line(xlim,[0 0]+gather(W(nnz(f))/W(1)),'linestyle',':','color','black');
-        legend({'singular vals.','sing. val. filter','noise floor'});
-        % residual norm plot
-        subplot(2,4,2); [k,~,v] = find(r); plot(k,v); ylabel('r / mad');
-        if opts.proj==0; xlim([0 nx*ny+1]); xlabel('dims'); end
-        if opts.proj==1; xlim([0 ny+1]); xlabel('dim 2'); end
-        if opts.proj==2; xlim([0 nx+1]); xlabel('dim 1'); end
-        line(xlim,[0 0]+opts.nmad,'linestyle','--','color','red'); axis tight;
-        line(xlim,[0 0],'linestyle','-','color','red'); title('residual plot');
-        % residual norm map
-        subplot(2,4,6); tmp = bsxfun(@times,data-ksp,mask); tmp = sum(abs(tmp),3);
-        if opts.proj==2; tmp = tmp'; end; imagesc(log(tmp)); title('residual map');
-        if opts.proj<=1; xlabel('dim 2'); ylabel('dim 1'); end
-        if opts.proj==2; xlabel('dim 1'); ylabel('dim 2'); end
-        % current image
-        subplot(2,4,[3 7]); tmp = sum(abs(ifft2(ksp)),3);
-        if tmp(1,1)>tmp(fix(nx/2),fix(ny/2)); tmp = fftshift(tmp); end;
-        imagesc(tmp); xlabel('dim 2'); ylabel('dim 1'); title(sprintf('iter %i',iter));
-        % change in norm and tol
-        subplot(2,4,[4 8]); g = plotyy(1:iter,tol,1:iter,normA,'semilogy','semilogy');
-        xlim(g,[0 iter+1]); xlabel('iters');
-        title('metrics'); legend({'||Δk||_F','||A||_*'}); drawnow;
+
+    % display progress - update every second or so
+    if iter==1
+        t(1) = tic; % global timer
+        t(2) = t(1); % display timer
+        t(3) = 0; % iter at each restart
+    elseif converged || toc(t(2)) > 1.5
+        if t(1)==t(2)
+            fprintf('Iterations per second: %.1f\n',(iter-1)/toc(t(1)));
+            disp('-----------------------------------------------------');
+            disp('Count  ||A||   Iter  Trimmed   rstd    noise.fl  Time');
+            disp('-----------------------------------------------------');
+        end
+        if converged && reject~=0
+           fprintf('%3i %9.2e %5i',numel(t)-3,nrm(1,iter),iter);
+            if isempty(opts.errors)
+                fprintf(' %6i  ',reject);
+            else
+                fprintf('%6i(%1i)',reject,ismember(reject,opts.errors));
+            end
+            fprintf('%9.2e %9.2e %5.0f\n',rstd,noise_floor,toc(t(1)));
+        end
+        display(W,f,r,ksp,data,iter,nrm,mask,opts); t(2) = tic;
     end
-    
-    % finish when nothing left to do
-    if converged==opts.minit && reject<=0
-        % show original image
-        subplot(2,4,[4 8]); tmp = sum(abs(ifft2(data)),3);
-        if tmp(1,1)>tmp(fix(nx/2),fix(ny/2)); tmp = fftshift(tmp); end
-        imagesc(tmp); xlabel('dim 2'); ylabel('dim 1'); title('iter 1');
-        break;
-    end
-    
+
+    % finish
+    if converged && reject==0; break; end
+
 end
 
 % return on CPU
@@ -309,8 +234,15 @@ flag = reject;
 function d = calc_sample_density(mask,opts);
 kernel = fftn(opts.kernel.mask,opts.dims(1:2));
 d = ifftn(bsxfun(@times,fftn(mask),kernel),'symmetric');
-d = circshift(d,-fix(size(opts.kernel.mask)/2));
+d = circshift(d,-floor(size(opts.kernel.mask)/2));
 d = max(d.*mask,0)/nnz(opts.kernel.mask);
+
+%% estimate noise std from data
+function noise_std = estimate_std(data,mask)
+tmp = bsxfun(@times,data,mask); tmp = nonzeros(tmp);
+tmp = sort([real(tmp); imag(tmp)]); % separate real/imag for median
+k = ceil(numel(tmp)/10); tmp = tmp(k:end-k+1); % trim 10% off both ends
+noise_std = 1.4826 * median(abs(tmp-median(tmp))) * sqrt(2); % robust std
 
 %% make calibration matrix
 function A = make_data_matrix(data,opts)
@@ -354,4 +286,35 @@ for k = 1:nk
     A(:,:,:,k,:) = circshift(A(:,:,:,k,:),-[x y]);
 end
 
-A = reshape(A,nx*ny,[]);
+A = reshape(A,nx,ny,nc,[]);
+
+%% show plots of various things (slow)
+function display(W,f,r,ksp,data,iter,nrm,mask,opts)
+[nx ny nc] = size(ksp);
+% prefer ims over imagesc
+if exist('ims','file'); imagesc = @(x)ims(x,-0.99); end
+% singular values
+subplot(2,4,[1 5]); plot(W/W(1)); hold on; plot(f,'--'); hold off
+xlim([0 numel(f)]); title(sprintf('rank %i/%i',nnz(f),numel(f)));
+line(xlim,[0 0]+gather(W(nnz(f))/W(1)),'linestyle',':','color','black');
+legend({'singular vals.','sing. val. filter','noise floor'});
+% residual norm plot
+subplot(2,4,2); [k,~,v] = find(r); plot(k,v); ylabel('r / std');
+if opts.readout==0; xlabel('dims'); end
+if opts.readout==1; xlabel('dim 2'); end
+if opts.readout==2; xlabel('dim 1'); end
+axis tight; line(xlim,[0 0]+opts.nstd,'linestyle','--','color','red');
+line(xlim,[0 0],'linestyle','-','color','red'); title('residual plot');
+% residual norm map
+subplot(2,4,6); tmp = sum(abs(data-ksp),3).*mask;
+if opts.readout==2; tmp = tmp'; end; imagesc(log(tmp)); title('residual map');
+if opts.readout<=1; xlabel('dim 2'); ylabel('dim 1'); end
+if opts.readout==2; xlabel('dim 1'); ylabel('dim 2'); end
+% current image
+subplot(2,4,[3 7]); tmp = sum(abs(ifft2(ksp)),3);
+if max(tmp(:,1))>max(tmp(:,floor(ny/2+1))); tmp = fftshift(tmp); end;
+imagesc(tmp); xlabel('dim 2'); ylabel('dim 1'); title(sprintf('iter %i',iter));
+% change in norms
+subplot(2,4,[4 8]); g = plotyy(1:iter,nrm(2,:),1:iter,nrm(1,:),'semilogy','semilogy');
+xlim(g,[0 iter]); xlabel('iters'); title('metrics'); legend({'||Δk||','||A||_*'});
+drawnow;
